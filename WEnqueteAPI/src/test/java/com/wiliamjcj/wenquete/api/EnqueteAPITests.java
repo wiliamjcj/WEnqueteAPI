4b347b652f8ba95830902bd45e2297c1bf5fbc60
wiljcj <wiljc@gmail.com>
2018-07-31 16:02:59 -0300

    Adicionando testes iniciais da API.

---
+++ WEnqueteAPI/src/test/java/com/wiliamjcj/wenquete/api/EnqueteAPITests.java
@@ -0,0 +1,48 @@
+package com.wiliamjcj.wenquete.api;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnqueteAPITests {
+
+	@Test
+	public void adicionarEnquete() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void listarTodasEnquetes() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void buscarEnquete() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void iniciarEnquete() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void votarEnquete() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void terminarEnquete() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void deletarEnquete() {
+		assertTrue(false);
+	}
+}
