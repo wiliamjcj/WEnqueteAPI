02d6b56759276b516700c56d2ed8b175b2bf6340
wiljcj <wiljc@gmail.com>
2018-07-31 15:20:22 -0300

    Criando projeto com springboot, com: web, jpa, flyway, h2 e postgresql.

---
+++ WEnqueteAPI/src/main/java/com/wiliamjcj/wenquete/WEnqueteApiApplication.java
@@ -0,0 +1,12 @@
+package com.wiliamjcj.wenquete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WEnqueteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WEnqueteApiApplication.class, args);
+	}
+}
